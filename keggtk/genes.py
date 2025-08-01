3b34b33f463dd14773e107259da093014257510b
matteobolner <matteo.bolner@studio.unibo.it>
2025-08-01 17:21:42 +0200

    Fixed function to split requests in blocks of 10 - api limits

--- keggtk/genes.py
+++ keggtk/genes.py
@@ -4,10 +4,18 @@ from keggtk.parsing import parse_sections
 
 def get_multiple_genes_text(gene_ids):
     url = "http://rest.kegg.jp/get/"
-    current_url = url + "+".join(gene_ids)
-    pathways = requests.get(current_url).text
-    pathways_dict = {i: j for i, j in zip(gene_ids, pathways.split("///\n\n"))}
-    return pathways_dict
+    all_genes_dict = {}
+    gene_ids = list(set(gene_ids))
+    # split in groups of 10 for API limits
+    for i in range(0, len(gene_ids), 10):
+        current_url = url + "+".join(gene_ids[i : i + 10])
+        genes = requests.get(current_url).text
+        genes_dict = {
+            i: j for i, j in zip(gene_ids[i : i + 10], genes.split("///\n\n"))
+        }
+        all_genes_dict.update(genes_dict)
+
+    return all_genes_dict
 
 
 def get_gene_symbols(text):
