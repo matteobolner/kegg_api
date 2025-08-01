87122718db894a1fc38112c834c8afc72134fccf
matteobolner <matteo.bolner@studio.unibo.it>
2025-08-01 17:16:01 +0200

    removed function exposure

--- keggtk/__init__.py
+++ keggtk/__init__.py
@@ -1,7 +1,8 @@
-from .pathways import (
-    get_pathways_list,
-    save_all_pathways,
-    get_multiple_pathways_text,
-    save_pathway_list,
-)
-from .reactions_xml import get_reactions_from_pathway
+# from .parsing import parse_sections
+# from .pathways import (
+#    get_pathways_list,
+#    save_all_pathways,
+#    get_multiple_pathways_text,
+#    save_pathway_list,
+# )
+# from .reactions_xml import get_reactions_from_pathway
