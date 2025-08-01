113a57fd3927ad4b1ff6ac557abb9686fe1b8213
matteobolner <matteo.bolner@studio.unibo.it>
2025-08-01 17:08:51 +0200

    Added gene specific functions

--- keggtk/genes.py
+++ keggtk/genes.py
@@ -1,25 +1,21 @@
 import requests
+from keggtk.parsing import parse_sections
 
 
-def get_genes_list(organism, get_pathway_file=False):
-    url = "http://rest.kegg.jp/list/genes/" + organism
-    data = requests.get(url)
-    pathways = data.text
-    pathways_text = data.text
-    pathways = pathways.split("\n")
-    pathways = filter(None, pathways)
-    pathway_dict = dict()
-    for path in pathways:
-        path = path.split("\t")
-        name = path[1]
-        pathid = re.search(r"(.*)", path[0]).group(1)
-        pathway_dict[pathid] = name
-    if get_pathway_file:
-        return pathway_dict, pathways_text
-    else:
-        return pathway_dict
+def get_multiple_genes_text(gene_ids):
+    url = "http://rest.kegg.jp/get/"
+    current_url = url + "+".join(gene_ids)
+    pathways = requests.get(current_url).text
+    pathways_dict = {i: j for i, j in zip(gene_ids, pathways.split("///\n\n"))}
+    return pathways_dict
 
 
-organism = "hsa"
-url = "http://rest.kegg.jp/list/genes/" + organism
-data = requests.get(url)
+def get_gene_symbols(text):
+    parsed = parse_sections(text)
+    return parsed["SYMBOL"][0].split(", ")
+
+
+def get_db_links(text):
+    parsed = parse_sections(text)
+    dblinks = {i.split(": ")[0]: i.split(": ")[1] for i in parsed["DBLINKS"]}
+    return dblinks
