38551ba63492043f1c54495dc8da68975aaa18b0
matteobolner <matteo.bolner@studio.unibo.it>
2025-08-03 15:43:07 +0200

    Fixed two reaction IDs in same line

--- keggtk/reactions_xml.py
+++ keggtk/reactions_xml.py
@@ -8,7 +8,6 @@ def get_reactions_from_pathway(pathway_id):
     data = requests.get(url)
     tree = ET.ElementTree(ET.fromstring(data.text))
     root = tree.getroot()
-
     entries = root.findall("entry")
     genes = []
     for e in entries:
@@ -19,10 +18,11 @@ def get_reactions_from_pathway(pathway_id):
             # symbols = e.findall("graphics")[0].get("name").split(",")[0]
             # gene["symbol"] = symbols.split(",")[0]
             genes.append(gene)
-
     genes = pd.DataFrame(genes)
     genes["gene"] = genes["gene"].str.split(" ")
     genes = genes.explode("gene")
+    genes["reaction"] = genes["reaction"].str.split(" ")
+    genes = genes.explode("reaction")
     genes = genes.drop_duplicates()
     # genes = genes[genes["reaction"].notna()]
     reaction_entries = root.findall("reaction")
@@ -37,13 +37,11 @@ def get_reactions_from_pathway(pathway_id):
         reaction["substrate"] = substrate
         reaction["product"] = product
         reactions.append(reaction)
-
     reactions = pd.DataFrame(reactions)
     if len(reactions) > 0:
         reactions["reaction"] = reactions["reaction"].str.split(" ")
         reactions = reactions.explode("reaction")
         reactions = reactions.drop_duplicates()
-
         df = reactions.merge(genes, on="reaction", how="outer")
         return df
     else:
