04604b3efd89b8632136711721dc569299c10704
matteobolner <matteo.bolner@studio.unibo.it>
2025-08-03 17:26:32 +0200

    Moved download genes to generic function

--- keggtk/genes.py
+++ keggtk/genes.py
@@ -1,26 +1,5 @@
-import requests
-from keggtk.parsing import parse_sections
+from keggtk.utils import parse_sections
 from tqdm import tqdm
-
-
-def get_multiple_genes_text(gene_ids):
-    url = "http://rest.kegg.jp/get/"
-    all_genes_dict = {}
-    gene_ids = list(set(gene_ids))
-    # split in groups of 10 for API limits
-    for gene_batch_indices in tqdm(range(0, len(gene_ids), 10)):
-        current_url = url + "+".join(
-            gene_ids[gene_batch_indices : gene_batch_indices + 10]
-        )
-        gene_batch_names = gene_ids[gene_batch_indices : gene_batch_indices + 10]
-        genes = requests.get(current_url).text
-
-        genes_dict = {
-            name: gene_text
-            for name, gene_text in zip(gene_batch_names, genes.split("///\n\n"))
-        }
-        all_genes_dict.update(genes_dict)
-    return all_genes_dict
 
 
 def get_gene_symbols(text):
---
+++ keggtk/utils.py
@@ -0,0 +1,41 @@
+import requests
+from tqdm import tqdm
+
+
+def download_multiple_ids_text(ids):
+    url = "http://rest.kegg.jp/get/"
+    all_ids_dict = {}
+    ids = list(set(ids))
+    # split in groups of 10 for API limits
+    for batch_indices in tqdm(range(0, len(ids), 10)):
+        current_url = url + "+".join(ids[batch_indices : batch_indices + 10])
+        batch_names = ids[batch_indices : batch_indices + 10]
+        text_results = requests.get(current_url).text
+
+        genes_dict = {
+            name: text for name, text in zip(batch_names, text_results.split("///\n\n"))
+        }
+        all_ids_dict.update(genes_dict)
+    return all_ids_dict
+
+
+def parse_sections(text):
+    """
+    Parse KEGG pathway text into sections
+    TODO: better parsing for BRITE and other layered sections
+    """
+    sections = {}
+    for i in text.split("\n"):
+        if i[:3] == "///":
+            continue
+        section = i[:12].strip()
+        section_data = i[12:].strip()
+        if section != "":
+            section_name = section
+            if section_name not in sections.keys():
+                sections[section_name] = []
+        sections[section_name].append(section_data)
+    # for k, v in sections.items():
+    #    if len(v) == 1:
+    #        sections[k] = v[0]
+    return sections
